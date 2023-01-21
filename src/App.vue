f4bd6c26b25ff4e774c05c344d06149733a3d9c5
NapatBourban <napatone123@gmail.com>
2023-01-21 13:29:55 +0700

    finish

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Box from "./components/BoxImg.vue";
+
+</script>
+
+<template>
+  
+  <div class="row">
+    <div v-for="n in 6" :key="n" class="col-4">
+    {{ n }}
+      <Box></Box>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ src/components/BoxImg.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LikeSad from "./LikeSad.vue";
+</script>
+
+<template>
+  <div class="card" style="width: 25rem ;">
+  <img src="../assets/ดาวน์โหลด.jpg" class="card-img-top" style=" width:25rem; height:30rem;">
+  <div class="card-body">
+    <p class="card-text">You will meet two kinds of people in life: ones who build you up and ones who tear you down. But in the end, you’ll thank them both.</p>
+    <LikeSad></LikeSad>
+  </div>
+</div>
+</template>
